7908b37e5545a7da8025a3d4b4b1f6e024d18abe
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2025-01-13 16:15:17 +0000

    Guest manager app done

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-weight = float(input("Enter your weight: "))
-
-unit = input("enter unit (Pound L(bs) 'l' or Kilogram K(g) 'k'): ")
-
-if unit.upper() == 'L':
-    converted = weight * 0.45
-    print("Your weight in Kilogram is" + str(converted) + "kg")
+guests = []
+quit = False
+while quit != True:
+    print(
+        "Welcome to the guest manager!\n. "
+        "1. Add a guest\n "
+        "2. Remove a guest\n "
+        "3. List all guests\n "
+        "4. Search for a guest\n "
+        "5. Quit"
+    )
     
-elif unit.upper() == 'K':
-    converted = weight // 0.45
-    print("Your weight in Pound is " + str(converted) + "lbs")
-    
-else:
-    print("Invalid input, please enter 'l' or 'k'")
+
+    choice = input("What would you like to do? ")
+
+    if choice == "1":
+        name = input("What is the guest's full  name? ")
+        guests.append(name)
+        print(name + " has been added.")
+    elif choice == "2":
+        name = input("What is the guest's full name? ")
+        guests.remove(name)
+        print(name + " has been removed.")
+    elif choice == "3":
+        if len(guests) == 0:
+            print("There are no guests yet, you need to add some. \n")
+        for guest in guests:
+            print(guest)
+    elif choice == "4":
+        name = input ("What is the guest's first or last name? ")
+        for guest in guests:
+            if name in guest:
+                print(guest)
+    elif choice == "5":
+        print("Exiting the guest manager...")
+        quit = True
+    else:
+        print("Invalid choice. Please try again.")
